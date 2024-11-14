14c6415afe623232eacc6e3f2855d726dbf8d1f1
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2024-11-14 15:19:10 +0600

    About buttons History, Mission

---
+++ src/app/About/History/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <p className="text-center text-3xl font-bold pt-5">
+        Services Page {">"} History Page
+      </p>
+    </div>
+  );
+};
+
+export default page;
--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-const AboutPage = () => {
-    return (
-        <div>
-                <p className="text-center text-3xl font-bold pt-5">About Page</p>
-        </div>
-    );
+const page = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <p className="text-center text-3xl font-bold pt-5">About Page</p>
+
+      <div className="flex gap-5 pt-5">
+        <Link href={"/About/History"}>
+          <button className="px-10 py-2 border border-white font-bold hover:bg-white hover:text-black">
+            History
+          </button>
+        </Link>
+        <Link href={"/About/Mission"}>
+          <button className="px-10 py-2 border border-white font-bold hover:bg-white hover:text-black">
+            Mission
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default AboutPage;
+export default page;
